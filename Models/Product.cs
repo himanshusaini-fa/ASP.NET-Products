e75b0957eab9dcf860137ba663d3f7c3e04d5b51
Himanshu <himanshusaini1100@gmail.com>
2020-08-12 12:19:25 +0530

    Added range to price and sellerId in Model

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,9 +11,9 @@ namespace Products.Models
         public string Description { get; set; }
         [Required, MinLength(3)]
         public string Category { get; set; }
-        [Required]
+        [Required, Range(1, double.MaxValue)]
         public double Price { get; set; }
-        [Required]
+        [Required, Range(1, int.MaxValue)]
         public int SellerId { get; set; }
         [Required, MinLength(2)]
         public string Brand { get; set; }
